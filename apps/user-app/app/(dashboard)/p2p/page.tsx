3ffb56b1590e93f0b986991fdb45dda0ffb03ac8
ar-ease <arghya0017@gmail.com>
2025-05-22 20:58:04 +0530

    added p2p transaction

---
+++ apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SendCard } from "../../../components/SendCard";
+
+const p2pPage = () => {
+  return (
+    <div className="w-full">
+      <SendCard />
+    </div>
+  );
+};
+
+export default p2pPage;
---
+++ apps/user-app/app/lib/actions/createOnRamptxn.ts
@@ -0,0 +1,27 @@
+"use server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth";
+import prisma from "@repo/db/client";
+
+export async function createOnRamptxn(amount: number, provider: string) {
+  const session = await getServerSession(authOptions);
+  const token = Math.random().toString();
+  const userId = session?.user.id;
+
+  if (!userId) {
+    return {
+      message: "user does not exist",
+    };
+  }
+
+  await prisma.onRampTransaction.create({
+    data: {
+      userId: Number(userId),
+      amount: amount,
+      status: "Processing",
+      startTime: new Date(),
+      provider,
+      token: token,
+    },
+  });
+}
