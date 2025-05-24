8876f6ec130e78a1a5d981174bc5666c08a0a9b7
ar-ease <arghya0017@gmail.com>
2025-05-24 11:23:31 +0530

    fixed for build

--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -1,16 +1,17 @@
-import { NextResponse } from "next/server"
-import { PrismaClient } from "@repo/db/client";
-
-const client = new PrismaClient();
+import { NextResponse } from "next/server";
+// import PrismaClient from "@repo/db/client";
+import prisma from "@repo/db/client";
 
 export const GET = async () => {
-    await client.user.create({
-        data: {
-            email: "asd",
-            name: "adsads"
-        }
-    })
-    return NextResponse.json({
-        message: "hi there"
-    })
-}
+  await prisma.user.create({
+    data: {
+      email: "asd",
+      name: "adsads",
+      number: "1234567890",
+      password: "securepassword", // Required field
+    },
+  });
+  return NextResponse.json({
+    message: "hi there",
+  });
+};
